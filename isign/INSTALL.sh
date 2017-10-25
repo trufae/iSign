5134e74bade4bdae57f05203c0395273ac696c04
Renan Berto <renanbertoo@gmail.com>
2017-10-25 15:01:52 -0200

    Added permission to install required packages

--- isign/INSTALL.sh
+++ isign/INSTALL.sh
@@ -250,8 +250,8 @@ mac_setup() {
 }
 
 linux_setup() {
-    apt-get install ideviceinstaller
-    apt-get install libimobiledevice-utils
+    apt-get install -y ideviceinstaller
+    apt-get install -y libimobiledevice-utils
 }
 
 
